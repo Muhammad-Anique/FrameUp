9e62cc0c142a0d24292f9dff2fa81f37cb3e5be5
MuhammadAnique2171 <l202171@lhr.nu.edu.pk>
2022-10-14 00:18:09 +0500

    Server : Creation --> Intial Files Created

---
+++ FrameUpServer/src/main/java/com/example/FrameUpServer/FrameUpServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.FrameUpServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrameUpServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrameUpServerApplication.class, args);
+	}
+
+}
---
+++ FrameUpServer/src/test/java/com/example/FrameUpServer/FrameUpServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FrameUpServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrameUpServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
