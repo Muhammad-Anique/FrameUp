ebea4cf088d37643854e351f2600f7f83da89033
MuhammadAnique2171 <l202171@lhr.nu.edu.pk>
2022-10-15 20:01:28 +0500

    Email Verfication: Created --> Class File + Trigger + Configs

--- FrameUpServer/src/main/java/com/example/FrameUpServer/FrameUpServerApplication.java
+++ FrameUpServer/src/main/java/com/example/FrameUpServer/FrameUpServerApplication.java
@@ -1,13 +1,29 @@
 package com.example.FrameUpServer;
 
+import com.example.FrameUpServer.Model.EmailServices.EmailSenderService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class FrameUpServerApplication {
 
+	@Autowired
+	private EmailSenderService service;
+
 	public static void main(String[] args) {
+
 		SpringApplication.run(FrameUpServerApplication.class, args);
+	}
+
+	@EventListener(ApplicationReadyEvent.class)
+	public void Trigger()
+	{
+		service.sendSimpleEmail("l202171@lhr.nu.edu.pk",
+				"OTP is 29990",
+				"Verification");
 	}
 
 }
